f4f2c6bdc367c661ac6f0b167f3253cf121c86ef
root <root@picard.hiven.host>
2018-04-04 05:08:07 +0000

    changes - tupac

--- templates_c/a587d6ea6902167f75eefe3fef9bdb6882804774_0.file.footer.tpl.php
+++ templates_c/a587d6ea6902167f75eefe3fef9bdb6882804774_0.file.footer.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.30, created on 2018-04-04 04:16:47
+/* Smarty version 3.1.30, created on 2018-04-04 04:49:58
   from "/home/nukeground/public_html/templates/footer.tpl" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.30',
-  'unifunc' => 'content_5ac451af251959_83904050',
+  'unifunc' => 'content_5ac45976e848b7_34967509',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'a587d6ea6902167f75eefe3fef9bdb6882804774' => 
     array (
       0 => '/home/nukeground/public_html/templates/footer.tpl',
-      1 => 1522815405,
+      1 => 1522817390,
       2 => 'file',
     ),
   ),
@@ -20,8 +20,36 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5ac451af251959_83904050 (Smarty_Internal_Template $_smarty_tpl) {
-echo '<script'; ?>
+function content_5ac45976e848b7_34967509 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<!-- Button trigger modal -->
+<div class='new-post-button'>
+  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+    <i class='fa fa-plus'></i>
+  </button>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+<?php echo '<script'; ?>
  src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"><?php echo '</script'; ?>
 >
 <?php echo '<script'; ?>
