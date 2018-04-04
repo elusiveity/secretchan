f7cda31c8eae6e14deea74927c715511394e30bf
root <root@picard.hiven.host>
2018-04-04 08:20:44 +0000

    Helps if you remove the fucking postblock from the fucking footer idiot

--- templates/postblock-floating.tpl
+++ templates/postblock-floating.tpl
@@ -12,7 +12,7 @@
 	<form class="modal-dialog" role="document">
 		<div class="modal-content">
 			<div class="modal-header">
-				<h5 class="modal-title" id="exampleModalLabel">New {if isset $parent}Thread{else}Reply{/if}</h5>
+				<h5 class="modal-title" id="exampleModalLabel">New Post</h5>
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 					<span aria-hidden="true">&times;</span>
 				</button>
--- templates_c/b132ea2854d6d9ae8a1f69c38e16236ff221a177_0.file.postblock-floating.tpl.php
+++ templates_c/b132ea2854d6d9ae8a1f69c38e16236ff221a177_0.file.postblock-floating.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.30, created on 2018-04-04 07:43:13
+/* Smarty version 3.1.30, created on 2018-04-04 07:49:02
   from "/home/nukeground/public_html/templates/postblock-floating.tpl" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.30',
-  'unifunc' => 'content_5ac482114635d9_50659869',
+  'unifunc' => 'content_5ac4836e605659_67471036',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'b132ea2854d6d9ae8a1f69c38e16236ff221a177' => 
     array (
       0 => '/home/nukeground/public_html/templates/postblock-floating.tpl',
-      1 => 1522827749,
+      1 => 1522828141,
       2 => 'file',
     ),
   ),
@@ -20,9 +20,10 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5ac482114635d9_50659869 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5ac4836e605659_67471036 (Smarty_Internal_Template $_smarty_tpl) {
 ?>
 <!-- Button trigger modal -->
+<!-- New Post Button -->
 <div class='new-post-button'>
   <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
     <i class='fa fa-plus'></i>
@@ -30,8 +31,9 @@ function content_5ac482114635d9_50659869 (Smarty_Internal_Template $_smarty_tpl)
 </div>
 
 <!-- Modal -->
+<!-- New Post Modal -->
 <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
+  <form class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
@@ -40,70 +42,13 @@ function content_5ac482114635d9_50659869 (Smarty_Internal_Template $_smarty_tpl)
         </button>
       </div>
       <div class="modal-body">
-      <form>
-			<div class='form-group row'>
-				<label for='options' class='col-2 col-form-label'>Options</label>
-				<div class='col-10'>
-					<input class='form-control' type='text' value='Anonymous' id='options' />
-				</div>
-			</div>
-			<div class='form-group row'>
-				<label for='subject' class='col-2 col-form-label'>Subject</label>
-				<div class='col-10'>
-					<input class='form-control' type='text' id='subject' />
-				</div>
-			</div>
-			<div class='form-group row'>
-				<label for='message' class='col-2 col-form-label'>Message</label>
-				<div class='col-10'>
-					<textarea class='form-control' type='text' id='message'></textarea>
-				</div>
-			</div>
-			<div class='form-group row'>
-				<label for='file' class='col-2 col-form-label'>File</label>
-				<div class='col-10'>
-					<input type='file' value='Anonymous' id='file' />
-				</div>
-			</div>
-			<p class='text-right'>
-				<button type='submit' class='btn btn-success'>Post</button>
-				<button type='button' class='btn btn-danger' data-toggle='collapse' data-target='.multi-collapse' aria-expanded='false' aria-controls='collapseExample'>Close</button>
-				<button type='button' class='btn btn-secondary' data-toggle='modal' data-target='#helpModal'>Help</button>
-			</p>
-			</div>
-		</form>
+        ...
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
         <button type="button" class="btn btn-primary">Save changes</button>
       </div>
     </div>
-  </div>
-</div>
-
-<!-- Modal -->
-<!-- div class="modal fade" id="helpModal" tabindex="-1" role="dialog" aria-labelledby="helpModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="helpModalLabel">Options Help</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <p class='text-left'>
-					Options are both seasonal and permanent. They can jazz up the look of your post, set your trips, return you to this post after posting and more. 
-				</p>
-				<p class='text-left'>
-					We only inform users of options using posts which are <b>not</b> stickied. There posts <b>will</b> be lost in time. So if you're not an oldfag you won't know what's going on.
-				</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
-</div -->
-<?php }
+  </form>
+</div><?php }
 }
